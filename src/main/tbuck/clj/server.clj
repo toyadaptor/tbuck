d34043c31a58c5ca2fcdbdb15b3187421e7c69e1
toyadaptor <adaptorwithtoy@gmail.com>
2024-08-30 07:24:26 +0900

    jws

--- src/main/tbuck/clj/server.clj
+++ src/main/tbuck/clj/server.clj
@@ -11,106 +11,48 @@
     [reitit.coercion.spec]
     [ring.adapter.jetty :as jetty]
     [ring.middleware.cors :refer [wrap-cors]]
+    [clj-time.core :as time]
     [buddy.auth :refer [authenticated?]]
-    ;[buddy.auth.backends.token :refer [jws-backend]]
+    [buddy.auth.backends.token :refer [jws-backend]]
     [buddy.core.nonce :as nonce]
     [buddy.core.codecs :as codecs]
-    [buddy.auth.backends.token :refer [token-backend]]
     [buddy.auth.middleware :refer [wrap-authentication wrap-authorization]]
     [buddy.sign.jwt :as jwt]))
 
 
-;(def secret "super-secret-key")
-
-;(def users
-;  {"user"  {:password "pass" :roles [:user]}
-;   "admin" {:password "adminpass" :roles [:admin]}})
-;
-;(defn generate-token [username]
-;  (jwt/sign {:user  username
-;             :roles (get-in users [username :roles])}
-;            secret))
-;
-;(defn authenticate [username password]
-;  (if-let [user (get users username)]
-;    (if (= password (:password user))
-;      (generate-token username)
-;      nil)))
-;(defn login [username password]
-;  (if-let [token (authenticate username password)]
-;    {:status 200
-;     :body   {:token token}}
-;    {:status 401
-;     :body   {:error "Invalid username or password"}}))
-
-(defn ok [d] {:status 200 :body d})
-(defn bad-request [d] {:status 400 :body d})
-(def authdata {:admin "secret"
-               :test  "secret"})
-
-(def tokens (atom {}))
+(def secret "hehe")
+(def auth-data {:admin "secret"})
 
 (defn random-token
   []
   (let [randomdata (nonce/random-bytes 16)]
     (codecs/bytes->hex randomdata)))
-(defn login
-  [username password]
-  (let [valid? (some-> authdata
+
+(defn login [username password]
+  (let [valid? (some-> auth-data
                        (get (keyword username))
                        (= password))]
     (if valid?
-      (let [token (random-token)]
-        (swap! tokens assoc (keyword token) (keyword username))
-        (ok {:token token}))
-      (bad-request {:message "wrong auth data"}))))
+      (let [claims {:user (keyword username)
+                    :exp  (time/plus (time/now) (time/seconds 3600))}
+            token (jwt/sign claims secret {:alg :hs512})]
+        {:status 200 :body {:token token}})
+      {:status 400 :body {:message "wrong auth data"}})))
 
 
-;(def backend
-;  (jws-backend {:secret secret
-;                :options {:alg :hs512}}))
-;
-;(def auth-backend
-;  (jws-backend {:secret "my-secret-key"
-;                :options {:alg :hs512}}))
-;
-;(defn wrap-auth [handler]
-;  (wrap-authentication handler auth-backend))
-;
-;(defn verify-token [token]
-;  (try
-;    (jwt/unsign token secret {:alg :hs512})
-;    (catch Exception e
-;      nil))) ;; 검증 실패 시 nil 반환
 
-;(defn my-authfn [req token]
-;  (println "my-authfn - " token)
-;  (when token
-;    (when-let [user (get @tokens (keyword token))]
-;      user)
-;    ))
-(defn my-authfn
-  [request token]
-  (let [token (keyword token)]
-    (get @tokens token nil)))
+(defn verify-token [token]
+  (try
+    (println "verify token - " token)
+    (get (jwt/unsign token secret {:alg :hs512}) :user)
+    (catch Exception e
+      nil)))                                                ;; 검증 실패 시 nil 반환
+
 
 (def auth-backend
-  (token-backend {:authfn my-authfn
-                  :token-name "Token"
-                  :optional? false
-                  :unauthorized-handler (fn [] {:status 401 :body "Unauthorized"})}))
+  (jws-backend {:secret secret :options {:alg :hs512}}))
 
-(comment
-  (let [token "asdf"]
-    (when-let [user (get @tokens (keyword token))]
-      user))
 
-  (my-authfn nil "af059835f20d2e5d12431424a98e162e")
-  (my-authfn nil "asdf")
-
-  (get @tokens (keyword "asdf"))
-  (swap! tokens assoc (keyword "asdf") (keyword "admin"))
-  @tokens)
 
 (def app-router
   (ring/router
@@ -121,9 +63,9 @@
                                        (login username password))}}]
       ["/main" {:get {:parameters {}
                       :responses  {200 {}}
-                      :handler    (fn [{:keys [identity]}]
+                      :handler    (fn []
                                     {:status 200
-                                     :body   {:identity identity} #_(api/main "main")})}}]
+                                     :body   (api/main "main")})}}]
       ["/tong/:tid/inouts" {:get  {:parameters {:path {:tid string?}}
                                    :handler    (fn [{{{:keys [tid]} :path} :parameters}]
                                                  (clojure.pprint/pprint tid)
@@ -188,8 +130,8 @@
                          rrm-parameter/parameters-middleware
                          rrc/coerce-exceptions-middleware
                          rrc/coerce-request-middleware
-                         rrc/coerce-response-middleware
-                         ]}}))
+                         rrc/coerce-response-middleware]}}))
+
 
 
 
@@ -200,21 +142,13 @@
     (ring/create-default-handler)))
 
 
-(defn require-authentication [handler]
-  (fn [req]
-    (if (some? (get-in req [:headers "authorization"]))
-      (handler req)
-      {:status 401 :body "Unauthorized"})))
+
 (def app
   (-> app-route
       (wrap-cors :access-control-allow-origin [#".*"]
                  :access-control-allow-methods [:get :post :put :delete :options]
                  :access-control-allow-headers ["Content-Type"])
-      (require-authentication)
-      (wrap-authentication auth-backend)
-
-      ))
-
+      (wrap-authentication auth-backend)))
 
 
 (defn start []
